59943962d817fd129e443d2b7b9acd537f12edca
Maxence Royer <maxenceroyer@hotmail.com>
2023-07-05 09:03:19 +0200

    [IMP] Add bundles feature for product.product and managae sale.order.line in terms of this product type

---
+++ musicalta_product/models/product_product.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    is_bundle = fields.Boolean(
+        string='Is a bundle'
+    )
+    bundle_id = fields.Many2one(
+        string='Is in bundle of',
+        comodel_name='product.product',
+        readonly=True,
+    )
+    product_bundle_ids = fields.One2many(
+        string='Bundle products',
+        comodel_name='product.product',
+        inverse_name='bundle_id',
+    )
+    is_in_bundle = fields.Boolean(
+        string='Is in product bundle',
+        compute='_compute_is_in_bundle',
+    )
+
+    def _compute_is_in_bundle(self):
+        bundles = self.search([('is_bundle', '=', True)])
+        for record in self:
+            record.is_in_bundle = False
+            for bundle in bundles:
+                if record.id in bundle.product_bundle_ids.ids:
+                    record.is_in_bundle = True
