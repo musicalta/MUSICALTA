a55ad9b9132cb0382f09d3eb9cc3626ec009b25b
olimalt <olimalt85@live.fr>
2024-01-05 18:29:16 +0100

    [FIX][I18N]musicalta_sale_loyalty : FIx the translation issue in lines

--- musicalta_sale_loyalty/models/sale_order.py
+++ musicalta_sale_loyalty/models/sale_order.py
@@ -5,13 +5,14 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _get_reward_values_discount(self, reward, coupon, **kwargs):
-        # call super to get original behavior
+        self.ensure_one()
         reward_values = super(SaleOrder, self)._get_reward_values_discount(
             reward, coupon, **kwargs)
 
         # modify the 'name' in each reward value
         for value in reward_values:
             if value.get('product_uom_qty') != 0:
-                value['name'] = _('Discount: %s' % reward.description)
+                value['name'] = _('Discount: %(desc)s',
+                                  desc=reward.with_context(lang=self.partner_id.lang).description)
 
         return reward_values
