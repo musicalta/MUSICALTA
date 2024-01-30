ad82b728a3c7f11d033fd031a7fc04c1158b4235
Charles-Edouard <charles-edouard.toutain@outlook.fr>
2024-01-30 18:15:07 +0100

    [ref] remove sql

--- payment_monetico/data/neutralize.sql
+++
@@ -1,8 +0,0 @@
--- disable monetico payment provider
-UPDATE payment_provider
-   SET monetico_key = NULL,
-       monetico_version = NULL,
-       monetico_TPE = NULL,
-       monetico_societe = NULL,
-       monetico_url_retour_ok = NULL,
-       monetico_url_retour_err = NULL;
