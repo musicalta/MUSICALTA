53e5121a75f70989b67150d2ecd187872508168d
olimalt <olimalt85@live.fr>
2024-02-26 17:27:36 +0100

    [UPGR]musicalta_product: 16.0.1.0.1 to add is_auditor in product

--- musicalta_product/__manifest__.py
+++ musicalta_product/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Product Specifics for Musicalta",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "summary": """Add product specifics for Musicalta.""",
     "category": "customization",
     "website": "https://irokoo.fr",
