4df77c354e6f068ffd2eda34dba2b16507bf0588
olimalt <olimalt85@live.fr>
2024-02-29 10:45:47 +0100

    [IMP]musicalta_sales : Add the payment method in the advanced payment wizard in sales

---
+++ musicalta_sales/wizard/sale_advance_payment_wzd.py
@@ -0,0 +1,49 @@
+# Copyright 2017 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import _, api, exceptions, fields, models
+from odoo.exceptions import UserError
+from odoo.tools import float_compare
+
+
+class AccountVoucherWizard(models.TransientModel):
+    _inherit = "account.voucher.wizard"
+
+    # == Payment methods fields ==
+    payment_method_line_id = fields.Many2one('account.payment.method.line', string='Payment Method',
+                                             readonly=False, store=True,
+                                             compute='_compute_payment_method_line_id',
+                                             domain="[('id', 'in', available_payment_method_line_ids)]",
+                                             help="Manual: Pay or Get paid by any method outside of Odoo.\n"
+                                             "Payment Providers: Each payment provider has its own Payment Method. Request a transaction on/to a card thanks to a payment token saved by the partner when buying or subscribing online.\n"
+                                             "Check: Pay bills by check and print it from Odoo.\n"
+                                             "Batch Deposit: Collect several customer checks at once generating and submitting a batch deposit to your bank. Module account_batch_payment is necessary.\n"
+                                             "SEPA Credit Transfer: Pay in the SEPA zone by submitting a SEPA Credit Transfer file to your bank. Module account_sepa is necessary.\n"
+                                             "SEPA Direct Debit: Get paid in the SEPA zone thanks to a mandate your partner will have granted to you. Module account_sepa is necessary.\n")
+    available_payment_method_line_ids = fields.Many2many(
+        'account.payment.method.line', compute='_compute_payment_method_line_fields')
+
+    @api.depends('payment_type', 'journal_id', 'currency_id')
+    def _compute_payment_method_line_fields(self):
+        for wizard in self:
+            if wizard.journal_id:
+                wizard.available_payment_method_line_ids = wizard.journal_id._get_available_payment_method_lines(
+                    wizard.payment_type)
+            else:
+                wizard.available_payment_method_line_ids = False
+
+    @api.depends('payment_type', 'journal_id')
+    def _compute_payment_method_line_id(self):
+        for wizard in self:
+            if wizard.journal_id:
+                available_payment_method_lines = wizard.journal_id._get_available_payment_method_lines(
+                    wizard.payment_type)
+            else:
+                available_payment_method_lines = False
+
+            # Select the first available one by default.
+            if available_payment_method_lines:
+                wizard.payment_method_line_id = available_payment_method_lines[0]._origin
+            else:
+                wizard.payment_method_line_id = False
