2040306693df91b172705d46d606ad1b4053b746
olimalt <olimalt85@live.fr>
2024-03-05 15:32:51 +0100

    [IMP]musicalta_sales : Add the link to sale order in the creation of payment linked to transactions

---
+++ musicalta_sales/models/account_payment.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    @api.model
+    def create(self, vals):
+        if vals.get('payment_transaction_id'):
+            payment_transaction = self.env['payment.transaction'].browse(
+                vals['payment_transaction_id']
+            )
+            if payment_transaction and payment_transaction.sale_order_id:
+                vals.update(
+                    {'sale_order_id': payment_transaction.sale_order_id[0].id})
+        return super(AccountPayment, self).create(vals)
