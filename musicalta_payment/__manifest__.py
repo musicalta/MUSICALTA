a53f2e42660bb53cefce5a0ceb5cbeee72c75c7c
olimalt <olimalt85@live.fr>
2024-05-14 16:24:28 +0200

    [ADD]musicalta_payment : Remove the button to redirect to user on payment page

---
+++ musicalta_payment/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Payments for Musicalta",
+    "version": "16.0.1.0.0",
+    "summary": """Remove the button in payments portal view.""",
+    "category": "customization",
+    "website": "https://irokoo.fr",
+    "author": "IROKOO, Olimalt Chahidov",
+    "license": "LGPL-3",
+    "depends": [
+        "payment",
+    ],
+    "data": [
+        # VIEWS
+        "views/payment_views.xml",
+    ],
+    "installable": True,
+}
